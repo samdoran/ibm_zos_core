32e2e35247fa42ec061100a2d937acec53ea038e
Rich Parker <richp405@gmail.com>
2021-01-11 09:44:38 -0500

    reprinting environment at copy.main

--- plugins/modules/zos_copy.py
+++ plugins/modules/zos_copy.py
@@ -1758,6 +1758,7 @@ def main():
         sftp_port=dict(arg_type="int", required=False),
         volume=dict(arg_type="str", required=False),
     )
+    print("before")
     pprint(module.params)  # local_charset and encoding.from are already messed up here.
     if (
         not module.params.get("encoding")
